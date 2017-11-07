4cc186da60a2356f651631ee7f43eaf2e07a8cac
vobile_lzl <824337531@qq.com>
2017-11-07 16:31:55 +0800

    #Comment  第一个scala工程

---
+++ scala_project_01/build.sbt
@@ -0,0 +1,6 @@
+name := "scala_project_01"
+
+version := "1.0"
+
+scalaVersion := "2.12.4"
+    
---
+++ scala_project_01/src/main/java/HelloScala.scala
@@ -0,0 +1,10 @@
+/**
+  * @Author: li_zhilei
+  * @Date: create in 15:58 17/11/7.
+  * @description:
+  */
+object HelloScala {
+  var vars="AAA"
+  val vals="BBB"
+  println(vals)
+}
