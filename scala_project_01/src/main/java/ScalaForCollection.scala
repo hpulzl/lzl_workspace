5e3122aeb114abcb0dd2017c0eda586ab4807ab2
hpulzl <824337531@qq.com>
2017-11-09 22:56:09 +0800

    #Comment scala的集合

--- scala_project_01/src/main/java/ScalaForCollection.scala
+++ scala_project_01/src/main/java/ScalaForCollection.scala
@@ -1,144 +1,41 @@
-
 /**
-  * Created by lzl on 2017/11/8.
-  * scala支持的集合操作
+  * Created by lzl on 2017/11/9.
+  * scala的集合
+  * 包括List、Set、Map等
   */
 object ScalaForCollection {
   def main(args: Array[String]): Unit = {
-//    println("====创建数组====")
-//    createArray()
-//    arrayFirst()
-//    println("====多维数组演示====")
-//    manyArray()
-//    println("====range数组演示====")
-//    rangeArray()
-    println("====fill数组演示====")
-    fillArray()
+//    listType()
+    createTable()
   }
 
   /**
-    * 多维数组
+    * 关于list集合的一些演示
     */
-  def manyArray(): Unit ={
-    /**
-      * 导入包
-      */
-    import Array._
-    var array = ofDim[Int](3,3)
-    var value=0
-    for (i <- 0 to 2){
-      for (j <- 0 to 2){
-        array(i)(j) = value;
-        value += 1
-      }
-    }
-
-    println("二维数组打印:")
-    for (arr <- array){
-      for (a <- arr){
-        print(" " + a)
-      }
-    }
-    println()
-    var arrayThree = ofDim[Int](3,3,4)
-    for (x <- 0 to 2){
-      for (y <- 0 to 2){
-        for (z <- 0 to 3){
-          arrayThree(x)(y)(z) = value
-          value += 1
-        }
-      }
-    }
-    println("打印三维数组:")
-    for (arrx <- arrayThree){
-      for (arry <- arrx){
-        for (arrz <- arry){
-          print(" " + arrz)
-        }
-      }
-    }
-    println()
-    println("concat 组合数组：")
-    var arry1 = Array(1,2,3)
-    var arry2 = Array(4,5,6)
-    var arry3 = concat(arry1,arry2)
-    for (arr <- arry3)
-      print(" " + arr)
+  def listType(): Unit ={
+    val listl : List[String] = List("A","B","C")
+    println(" list is ")
+    for (l <- listl)
+      print(" " + l)
+    //空的list
+    val empty : List[Nothing] = List()
+    println(" empty list is " + empty)
+    //二维列表
+    val dim : List[List[String]] = List(
+      List("A","B","C"),List("D","E","F"),List("G","H","I"))
+    println(dim.mkString(" "))
   }
 
-  def rangeArray(): Unit ={
-    import Array._
-    val array1 = range(10,19,2)
-    val array2 = range(200,10)
-
-    println("range step = 2 打印:")
-    for (arr <- array1)
-      print(" " + arr)
-
-    println()
-    println("range step = 1 打印:")
-    for (arr <- array2)
-      print(" " + arr)
-  }
-  def fillArray(): Unit ={
-    /**
-      * 返回一个数组长度为10的数组，数组每个值为2
-      */
-    val array = Array.fill(10)(3)
-    for (arr <- array)
-      print(" " + arr)
-  }
   /**
-    * 1. 数组必须用val来修饰，表示不可变的数组
-    * 2. `new Array[String](10)` 是用来创建一个String类型的数组，并开辟长度为10的空间
+    * 使用 :: 方法创建数组
+    * Nil 的概念
+    *
     */
-  def createArray(): Unit ={
-    var arrays = new Array[String](10)
-    for (a <- arrays)
-      print(a+" ")
-    var array2 = Array
-
-
-  }
-
-  def arrayFirst(): Unit ={
-    println("打印一串数组，声明其类型，方式一:")
-    //@1 创建方式一
-    val arrays = new Array[String](10)
-    arrays(0) = "A"
-    arrays(1) = "B"
-    for (item <- arrays) print(" " + item)
-
-    println("打印一串数组，自动推到类型，方式二:" + "\n")
-    val arrays2 = Array(1,2,3,4,5)
-    for (item <- arrays2) print(" " + item)
-    println("\nlength is " + arrays2.length)
-
-    println("打印一串数组，使用apply创建数组，方式二:" + "\n")
-    val arrays3 = Array.apply("scala","Spark","kafka")
-    for (item <- arrays3) print(" " + item)
-//    print("iterator 遍历：" + arrays3.toIterator)
-
-//    apply(1)
-    println("使用ArrayBuffer类创建数组:")
-    arrayToBuffer()
-  }
-
- //TODO 需要弄明白这种返回是什么意思?
-  def apply(i: Int): Int = throw new Error()
-  /**
-    * 创建一个可变数组的方法
-    */
-  def arrayToBuffer(): Unit ={
-    //导入ArrayBuffer的包
-    import scala.collection.mutable.ArrayBuffer
-    //这里写new 不写new 都可以吗?
-    var arrayBuffers = ArrayBuffer[Int]()
-    //var arrayBuffers = new  ArrayBuffer[Int]()
-    arrayBuffers += 1
-    arrayBuffers += 2
-    arrayBuffers += 3
-    println(arrayBuffers.toString)
-
+  def createTable(): Unit ={
+    val site = "scala" :: ("java" :: ("spark" :: Nil)) ::
+                "A" :: ("B" :: ("C" :: Nil))
+      print(site.mkString(","))
+    val empty = List
+    print(empty)
   }
 }
